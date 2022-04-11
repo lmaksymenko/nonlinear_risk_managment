4652d33633c0363fd31620d8d33258fadabf252e
Eden Luvishis <eden.luvishis@gmail.com>
2022-04-11 12:22:30 -0400

    eden got off her ass and is doing reserach

--- data_generation.R
+++ data_generation.R
@@ -45,18 +45,27 @@ gen_manual_data <- function(){
 #using https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html
 
 gen_calibrated_data <- function(){
-  data = read.csv('data/ff_factors.csv', header = TRUE)
+  data = read.csv('data/monthlyffdata.csv', header = TRUE)
+  colnames(data)
+  #remove risk free rate
+  if(c("RF") %in% colnames(data)){
+    data = data[, -which(names(data) == "RF")]
+  }
   colnames(data) = c(1:length(data))
   
-  cov_mat = cov(data)
-  means = unlist(lapply(data, mean))
+  cov(data)
+  #divide by 100 because the data is formatted as monthly and as a percent (eg: 2.58%)
+  cov_mat = cov(data)/20/100
+  means = unlist(lapply(data, mean))/20/100
   #sds = lapply(data, sd)
   num = 1000
   
+  
   raw = mvrnorm(n = num, mu = means, Sigma = cov_mat)
+  
   # we need to add noise
-  eps = rnorm(n = num, sd = 0.1)
-  Y = 0.25*raw[,1] + 0.25*raw[,2]^2 + 0.25*raw[,3] + 0.25*raw[,4] + eps
+  eps = rnorm(n = num, sd = 0.001)
+  Y = 0.25*raw[,1] + 0.25*raw[,2] + 0.25*raw[,3] + 0.25*raw[,4] + eps
   
   raw_df = data.frame(raw, Y)
   
